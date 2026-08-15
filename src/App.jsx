994b7d9c436a3470765df26599b30932ab4c5c66
Lakshan Sandeepa <lakshansandeepa0305@gmail.com>
2026-08-15 07:00:06 +0530

    feat: restructure App component layout and add placeholder for portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+// import React from "react";
+// import Footer from "./components/Footer";
+// import Hero from "./components/Hero";
+
+// export default function App() {
+//   return (
+//     <div className="min-h-screen bg-dark text-white selection:bg-white selection:text-black">
+//       <main>
+//         <Hero />
+//       </main>
+//       <Footer />
+//     </div>
+//   );
+// }
+
+import React from "react";
+import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
 export default function App() {
   return (
-    <>
+    <div className="bg-[#0a0a0a] min-h-screen text-white select-none">
+      {/* 1. Hero Section */}
+      <Hero />
+
+      {/* 2. Portfolio Section  */}
+      <main className="w-full">
+        <div className="h-[50vh] flex items-center justify-center border-b border-neutral-900 font-mono text-xs text-neutral-600">
+          [PORTFOLIO SECTION WILL BE PLACED HERE]
+        </div>
+      </main>
+
+      {/* 3. Footer Section */}
       <Footer />
-    </>
+    </div>
   );
 }
